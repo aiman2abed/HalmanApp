7992dcf8e424b7098f1044c42296b72d8b328a5f
DarkLordF1 <aaimanabed96@gmail.com>
2026-04-05 23:31:32 +0300

    added logo variants for mobile homescreen button

--- frontend/src/app/manifest.ts
+++ frontend/src/app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#f97316', // لون شريط الإشعارات العلوي في الهاتف (برتقالي)
     icons: [
       {
-        src: '/icon-192x192.png',
+        src: '/assets/icon-192x192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/icon-512x512.png',
+        src: '/assets/icon-512x512.png',
         sizes: '512x512',
         type: 'image/png',
       },
